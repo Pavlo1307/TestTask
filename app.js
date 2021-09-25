8887fb6936b5bc810664dbb8eaf91709cf0644e9
Pavlo1307 <pavloshavel@gmail.com>
2021-09-25 14:23:05 +0300

    something do

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const { PORT } = require('./config/variables')
+
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log('App listen 5000')
+})
